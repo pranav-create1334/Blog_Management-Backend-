48bd509981adb0f2404da5514247a57c73b93bac
Pranav <pranavkusalkar39@gmail.com>
2026-02-22 19:56:24 +0530

    Unit test for the : Blog Repository

--- src/test/java/com/example/blog/repository/BlogRepositoryTest.java
+++ src/test/java/com/example/blog/repository/BlogRepositoryTest.java
@@ -1,37 +1,37 @@
-//package com.example.blog.repository;
-//
-//import com.example.blog.entity.Blog;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.test.annotation.Rollback;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//
-//@ActiveProfiles("test")
-//@DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@Rollback(false)
-//public class BlogRepositoryTest {
-//
-//    @Autowired
-//    private BlogRepository blogRepository;
-//
-//    @Test
-//    void saveBlogTest() {
-//// Arrange
-//        Blog blog = new Blog();
-//        blog.setTitle("Test Blog");
-//        blog.setContent("This is test content");
-//// Act
-//        Blog saved = blogRepository.save(blog);
-////Assert
-//        assertThat(saved.getId()).isNotNull();
-//        assertThat(saved.getTitle()).isEqualTo("Test Blog");
-//    }
-//}
+package com.example.blog.repository;
+
+import com.example.blog.entity.Blog;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class BlogRepositoryTest {
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+    @Test
+    void saveBlogTest() {
+// Arrange
+        Blog blog = new Blog();
+        blog.setTitle("Test Blog");
+        blog.setContent("This is test content");
+// Act
+        Blog saved = blogRepository.save(blog);
+//Assert
+        assertThat(saved.getId()).isNotNull();
+        assertThat(saved.getTitle()).isEqualTo("Test Blog");
+    }
+}
